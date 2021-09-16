4658c1d87939961a5b632d75b88eb44b6c474081
Marie Drake <marie.drake@zoopla.co.uk>
2021-09-16 16:34:16 +0100

    update test

--- cypress/integration/cypress-image-snapshot/visual-snapshot.spec.js
+++ cypress/integration/cypress-image-snapshot/visual-snapshot.spec.js
@@ -2,10 +2,19 @@
 
 describe('Image search with Mock API', () => {
     it('should look ok visually with static data', () => {
-        cy.intercept('search/photos?query=mock+', { fixture: 'unsplash.json' });
+        cy.intercept('photos?page=1').as('defaultSearchResults');
 
-        cy.visit('/');
-        cy.get('[data-testid="search-input"]').type('mock {enter}');
+        cy.visit('/').wait('@defaultSearchResults', {
+            timeout: Cypress.config('defaultCommandTimeout'),
+        });
+
+        cy.intercept('search/photos?query=mock+', { fixture: 'unsplash.json' }).as('mockedResults');
+
+        cy.get('[data-testid="search-input"]')
+          .type('mock {enter}')
+          .wait('@mockedResults', {
+            timeout: Cypress.config('defaultCommandTimeout'),
+        });
 
         cy.matchImageSnapshot();
     });
